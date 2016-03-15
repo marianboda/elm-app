3eaaf384a2d597e7fa44e419b504f182fd0e237e
Marian Boda <marianboda@gmail.com>
2016-03-15 11:26:12 +0100

    Added StartApp to the stack

--- Main.elm
+++ Main.elm
@@ -2,18 +2,16 @@ module Main (..) where
 
 import Html exposing (Html)
 import Html.Events as Events
-import Mouse
-import Keyboard
 import Debug
-import String
-import Char
+
+import StartApp.Simple
 
 type alias Model =
   { chars: String, count: Int }
 
 type Action
   = NoOp
-  | MouseClick Int
+  | Increase
   | KeyPress Int
 
 initialModel : Model
@@ -28,41 +26,22 @@ view address model =
     [
       Html.div [] [Html.text (toString model.count)]
     , Html.button
-        [ Events.onClick address (MouseClick 1) ]
+        [ Events.onClick address Increase ]
         [ Html.text "Click"]
     ]
 
 update : Action -> Model -> Model
 update action model =
   case action of
-    MouseClick amount ->
-      { model | count = model.count + amount}
-    KeyPress key ->
-      Debug.log (toString key)
-      { model | chars = String.concat [model.chars, String.fromChar (Char.fromCode key)]}
+    Increase -> { model | count = model.count +2 }
     NoOp -> model
-
-mouseClickSignal : Signal.Signal Action
-mouseClickSignal = Signal.map (\_ -> MouseClick 2) Mouse.clicks
-
-keyPressSignal : Signal.Signal Action
-keyPressSignal = Signal.map (\key -> KeyPress key) Keyboard.presses
-
-actionSignal : Signal.Signal Action
-actionSignal = Signal.merge mb.signal keyPressSignal
-
-mb : Signal.Mailbox Action
-mb = Signal.mailbox NoOp
-
-
-
-modelSignal : Signal.Signal Model
-modelSignal =
-  Signal.foldp
-    update
-    initialModel
-    actionSignal
+    _ -> model
 
 main : Signal.Signal Html
 
-main = Signal.map (view mb.address) modelSignal
+main =
+  StartApp.Simple.start
+    { model = initialModel
+    , view = view
+    , update = update
+    }
