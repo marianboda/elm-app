8ab38db8f87e2753948ef8817de1ee333ee98613
Marian Boda <marianboda@gmail.com>
2016-03-23 13:06:46 +0100

    Added pages for router

--- player-app/src/Main.elm
+++ player-app/src/Main.elm
@@ -5,9 +5,9 @@ import Effects exposing ( Effects, Never )
 import Task
 import StartApp
 
-import Actions exposing ( Action )
+import Routing
+import Actions exposing (..)
 import Models exposing (..)
-
 import Update exposing (..)
 import View exposing (..)
 
@@ -20,8 +20,13 @@ app =
     { init = init
     , view = view
     , update = update
-    , inputs = []
+    , inputs = [ routerSignal ]
     }
+
+routerSignal : Signal Action
+routerSignal =
+  Signal.map RoutingAction Routing.signal
+
 
 main : Signal.Signal Html
 main = app.html
@@ -29,3 +34,7 @@ main = app.html
 port runner : Signal (Task.Task Never ())
 port runner =
   app.tasks
+
+port routeRunTask : Task.Task () ()
+port routeRunTask =
+  Routing.run
--- player-app/src/View.elm
+++ player-app/src/View.elm
@@ -3,15 +3,52 @@ import Html exposing (..)
 
 import Actions exposing (..)
 import Models exposing (..)
+import Routing
 import Players.List
+import Players.Edit
+import Players.Models exposing (PlayerId)
 
 view : Signal.Address Action -> AppModel -> Html
 view address model =
-  div [] [ page address model ]
+  let
+    _ = Debug.log "model" model
+  in
+    div [] [ page address model ]
 
 page : Signal.Address Action -> AppModel -> Html
 page address model =
+  case model.routing.route of
+    Routing.PlayersRoute ->
+      playersPage address model
+    Routing.PlayerEditRoute playerId ->
+      playerEditPage address model playerId
+    Routing.NotFoundRoute ->
+      notFoundView
+
+playersPage : Signal.Address Action -> AppModel -> Html.Html
+playersPage address model =
   let
     viewModel = { players = model.players }
   in
     Players.List.view (Signal.forwardTo address PlayersAction) viewModel
+
+playerEditPage : Signal.Address Action -> AppModel -> PlayerId -> Html.Html
+playerEditPage address model playerId =
+  let
+    maybePlayer =
+      model.players
+        |> List.filter (\player -> player.id == playerId)
+        |> List.head
+  in
+    case maybePlayer of
+      Just player ->
+        let
+          viewModel = { player = player }
+        in
+          Players.Edit.view (Signal.forwardTo address PlayersAction) viewModel
+      Nothing ->
+        notFoundView
+
+notFoundView : Html.Html
+notFoundView =
+  div [] [ text "Not found" ]
